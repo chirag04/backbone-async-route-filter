7608edbbad58e5bc6d136fcfa997520cb877d073
chirag04 <jain_chirag04@yahoo.com>
2013-08-29 13:49:25 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+Backbone Async Route filters
+==================
+
+Backbone Async Route filters allows you to have a pre-condition for the router using `before` filters and some
+"after" routing calls using `after` filters. 
+
+Backbone async filters is port of [fantactuka/backbone-route-filter](https://github.com/fantactuka/backbone-route-filter) with expressjs style async next callbacks.
+
+## Installation
+Using [Bower](http://twitter.github.com/bower/) `bower install backbone-async-route-filter` or just copy [backbone-async-route-filter.js](https://raw.github.com/chirag04/backbone-async-route-filter/master/backbone-async-route-filter.js)
+
+## Usage
+
+```js
+var Router = Backbone.Router.extend({
+  routes: {
+    'users': 'usersList',
+    'users/:id': 'userShow',
+    'account/sign-in': 'signIn'
+  },
+
+  before: {
+    // Using instance methods
+    'users(:/id)': 'checkAuthorization',
+
+    // Using inline filter definition
+    '*any': function(fragment, args, next) {
+      console.log('Atempting to load ' + fragment + ' with arguments: ', args);
+      next();
+    }
+  },
+
+  after: {
+    // Google analytics tracking
+    // After filter will be triggered only if all before filters passed and action was triggered,
+    // so you'll only track pages that was displayed to user
+    '*any': function(fragment, args, next) {
+      goog._trackPageview(fragment);
+      next();
+    }
+  },
+
+  checkAuthorization: function(fragment, args, next) {
+    
+    //make ajax to check authorization here.
+    $.ajax({
+      data: somedata,
+      success: function() {
+        // if logged in execute next() to move ahead.
+        next();
+      },
+      error: function() {
+        //redirect to signin page.
+      }
+    });
+
+  }
+});
+```
+
+## LICENSE
+
+MIT
---
+++ backbone-route-filter.js
@@ -0,0 +1,82 @@
+(function(Backbone, _) {
+
+  var extend = Backbone.Router.extend;
+
+  Backbone.Router.extend = function() {
+    var child = extend.apply(this, arguments),
+      childProto = child.prototype,
+      parentProto = this.prototype;
+
+    _.each(['before', 'after'], function(filter) {
+      _.defaults(childProto[filter], parentProto[filter]);
+    });
+
+    return child;
+  };
+
+  _.extend(Backbone.Router.prototype, {
+
+    /**
+     * Override default route fn to call before/after filters
+     *
+     * @param {String} route
+     * @param {String} name
+     * @param {Function} [callback]
+     * @return {*}
+     */
+    route: function(route, name, callback) {
+      if (!_.isRegExp(route)) route = this._routeToRegExp(route);
+      if (_.isFunction(name)) {
+        callback = name;
+        name = '';
+      }
+      if (!callback) callback = this[name];
+      var router = this;
+      
+      // store all the before and after routes in a stack
+      var beforeStack = [];
+      var afterStack = [];
+      var index = 0;
+      _.each(router.before, function(value,key) {
+        beforeStack.push({'filter':key, 'filterFn':value});
+      });
+      _.each(router.after, function(value,key) {
+        afterStack.push({'filter':key, 'filterFn':value});
+      });
+
+      Backbone.history.route(route, function(fragment) {
+        var args = router._extractParameters(route, fragment);
+        
+        function next(stack, runRoute) {
+          var layer = stack[index++];
+          if(layer) {
+            var filter = _.isRegExp(layer.filter) ? layer.filter : router._routeToRegExp(layer.filter);
+            if(filter.test(fragment)) {
+              var fn = _.isFunction(layer.filterFn) ? layer.filterFn : router[layer.filterFn];
+              fn.apply(router, [fragment, args, function(){ next(stack, runRoute); } ]);
+            } else {
+              next(stack, runRoute);
+            }
+          } else if(runRoute) {
+            callback.apply(router, args);
+          }
+        }
+
+        // start with top of the before stack
+        next(beforeStack, true);
+        
+        router.trigger.apply(router, ['route:' + name].concat(args));
+        router.trigger('route', name, args);
+        Backbone.history.trigger('route', router, name, args);
+        
+        // reset the stack coutner for after filters
+        index = 0;
+        next(afterStack);
+
+      });
+
+      return this;
+    }
+
+  });
+})(Backbone, _);
